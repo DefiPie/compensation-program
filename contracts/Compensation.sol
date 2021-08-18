c5875e3b3d26a23be9034188578d624ee35df010
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-08-18 10:58:12 +0300

    save progress

--- contracts/Compensation.sol
+++ contracts/Compensation.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.4;
 
 import "./tokens/ERC20.sol";
+import "./interfaces/Interfaces.sol";
 import "./Blacklist.sol";
 
 contract Compensation is BlackList {
@@ -10,11 +11,23 @@ contract Compensation is BlackList {
     uint public startBlock;
     uint public removeBlocks = 1203664; // 0,5 year in blocks for eth
 
-    mapping(address => uint) pTokens;
+    address public controller;
+    address public ETHUSDPriceFeed;
 
-    constructor(address stableCoin_, uint startBlock_, uint removeBlocks_) {
+    mapping(address => uint) public pTokens;
+
+    struct Balance {
+        uint amount;
+        uint out;
+    }
+
+    mapping(address => Balance) public balances;
+
+    constructor(address stableCoin_, uint startBlock_, uint removeBlocks_, address controller_, address ETHUSDPriceFeed_) {
         require(
-            stableCoin != address(0),
+            stableCoin != address(0)
+            && controller_ != address(0)
+            && ETHUSDPriceFeed_ != address(0),
             "Compensation::Constructor: address is 0"
         );
 
@@ -33,6 +46,9 @@ contract Compensation is BlackList {
 
         startBlock = startBlock_;
         removeBlocks = removeBlocks_;
+
+        controller = controller_;
+        ETHUSDPriceFeed = ETHUSDPriceFeed_;
     }
 
     function addPToken(address pToken, uint price) public onlyOwner returns (bool) {
@@ -55,7 +71,21 @@ contract Compensation is BlackList {
         return true;
     }
 
-    function compensation(address pToken, uint amount) public returns (bool) {
+    function compensation(address pToken, uint pTokenAmount) public returns (bool) {
+        require(block.number > startBlock, "Convert::convert: you can convert pTokens after start block only");
+
+        uint sumBorrow = calcAccountBorrow(msg.sender);
+
+        if (sumBorrow != 0) {
+            uint ETHUSDPrice = uint(AggregatorInterface(ETHUSDPriceFeed).latestAnswer());
+            uint loan = sumBorrow * ETHUSDPrice / 1e8 / 1e18; // 1e8 is chainlink, 1e18 is eth
+
+            require(loan < 1, "Convert::convert: sumBorrow must be less than $1");
+        }
+
+        uint amount = doTransferIn(msg.sender, pToken, pTokenAmount);
+
+        balances[msg.sender].amount += calcCompensationAmount(pToken, amount);
 
         return true;
     }
@@ -63,6 +93,37 @@ contract Compensation is BlackList {
     function calcCompensationAmount(address pToken, uint amount) public returns (uint) {
 
         return 0;
+    }
+
+    function claimToken() public returns (bool) {
+        require(block.number > startBlock, "Convert::claimToken: bad timing for the request");
+        require(!isBlackListed[msg.sender], "Convert::claimToken: user in black list");
+
+        uint amount = balances[msg.sender].amount - balances[msg.sender].out;
+
+        balances[msg.sender].out += amount;
+
+        doTransferOut(stableCoin, msg.sender, amount);
+
+        return true;
+    }
+
+    function calcAccountBorrow(
+        address account
+    ) public view returns (uint) {
+        uint sumBorrow;
+
+        address[] memory assets = ControllerInterface(controller).getAssetsIn(account);
+        for (uint i = 0; i < assets.length; i++) {
+            address asset = assets[i];
+
+            uint borrowBalance = PTokenInterface(asset).borrowBalanceStored(account);
+            uint price = ControllerInterface(controller).getOracle().getUnderlyingPrice(asset);
+
+            sumBorrow += price * borrowBalance;
+        }
+
+        return sumBorrow;
     }
 
     function doTransferIn(address from, address token, uint amount) internal returns (uint) {
