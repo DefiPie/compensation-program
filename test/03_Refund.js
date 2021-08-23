83968e22d5b5a647692438bb9d64834bee0e2032
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-08-23 16:17:11 +0300

    init tests for refund

--- test/03_Refund.js
+++ test/03_Refund.js
@@ -1,11 +1,55 @@
 const { expect } = require("chai");
+const { ethers } = require('hardhat');
 
 describe("Refund", function () {
+    let refund, Refund;
+    let mock, Mock, ERC20Token;
 
+    let controller;
+    let ETHUSDPriceFeed;
+
+    let refund_startBlock = '10';
+    let refund_endBlock = '100';
+
+    let owner;
+
+    before(async () => {
+        Mock = await hre.ethers.getContractFactory("Mock");
+        ERC20Token = await hre.ethers.getContractFactory("ERC20Token");
+        Refund = await hre.ethers.getContractFactory("Refund");
+
+        [owner] = await ethers.getSigners();
+    });
+
+    beforeEach(async () => {
+        mock = await Mock.deploy();
+        console.log("Mock deployed to:", mock.address);
+
+        controller = mock.address;
+        ETHUSDPriceFeed = mock.address;
+
+        refund = await Refund.deploy(
+            refund_startBlock,
+            refund_endBlock,
+            controller,
+            ETHUSDPriceFeed,
+        );
+        console.log("Refund deployed to:", refund.address);
+    });
 
     describe('Transactions', async () => {
-        it('deploy Refund and check data', async () => {
+        it('check deploy data', async () => {
+            const [startBlock, endBlock, contractController, contractETHUSDPriceFeed] = await Promise.all([
+                refund.startBlock(),
+                refund.endBlock(),
+                refund.controller(),
+                refund.ETHUSDPriceFeed()
+            ]);
 
+            expect(startBlock).to.be.equal(refund_startBlock);
+            expect(endBlock).to.be.equal(refund_endBlock);
+            expect(contractController).to.be.equal(controller);
+            expect(contractETHUSDPriceFeed).to.be.equal(ETHUSDPriceFeed);
         });
     });
 });
