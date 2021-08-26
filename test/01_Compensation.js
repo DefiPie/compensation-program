7bd83683d52edfb8abd6168c912c05f4e3bc4090
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-08-26 16:11:59 +0300

    add checkpoints tests for compensation

--- test/01_Compensation.js
+++ test/01_Compensation.js
@@ -5,6 +5,7 @@ const { eventsName, revertMessages } = require('./shared/enums');
 describe("Compensation", function () {
     let compensation, Compensation;
     let mainMock, MainMock, ERC20Token, MockPToken;
+    let stable;
 
     let controller;
     let ETHUSDPriceFeed;
@@ -32,7 +33,7 @@ describe("Compensation", function () {
         ETHUSDPriceFeed = mainMock.address;
 
         let amount = '1000000000000'; // 1,000,000e6
-        const stable = await ERC20Token.deploy(
+        stable = await ERC20Token.deploy(
             amount,
             'USDT',
             'USDT'
@@ -188,7 +189,54 @@ describe("Compensation", function () {
             expect(contractPrice3.toString()).to.be.equal(price3);
 
             // 3. add 3 checkpoint
+            let stableAmount1 = '1000000000'; // 100e6
+            let stableAmount2 = '3000000000'; // 300e6
+            let stableAmount3 = '7000000000'; // 700e6
+
+            await expect(
+                compensation.connect(accounts[0]).addCheckpoint(stableAmount1)
+            ).to.be.revertedWith(revertMessages.ownableCallerIsNotOwner);
+
+            await stable.approve(compensation.address, stableAmount1);
+
+            const ownerBalanceBefore = await stable.balanceOf(owner.address);
+            const refundContractBalanceBefore = await stable.balanceOf(compensation.address);
+
+            await compensation.addCheckpoint(stableAmount1);
+
+            const ownerBalanceAfter = await stable.balanceOf(owner.address);
+            const refundBalanceAfter = await stable.balanceOf(compensation.address);
+
+            expect(ownerBalanceBefore.sub(ownerBalanceAfter).toString()).to.be.equal(stableAmount1);
+            expect(refundBalanceAfter.sub(refundContractBalanceBefore).toString()).to.be.equal(stableAmount1);
+
+            let checkpointLength = await compensation.getCheckpointsLength();
+            expect(checkpointLength).to.be.equal(1);
+
+            let checkpoints = await compensation.checkpoints(0);
+            expect(checkpoints.toString()).to.be.equal(stableAmount1);
+
+            await stable.approve(compensation.address, stableAmount2);
+            await compensation.addCheckpoint(stableAmount2);
+
+            checkpointLength = await compensation.getCheckpointsLength();
+            expect(checkpointLength).to.be.equal(2);
+
+            checkpoints = await compensation.checkpoints(1);
+            expect(checkpoints.toString()).to.be.equal(stableAmount2);
+
+            await stable.approve(compensation.address, stableAmount3);
+            await compensation.addCheckpoint(stableAmount3);
+
+            checkpointLength = await compensation.getCheckpointsLength();
+            expect(checkpointLength).to.be.equal(3);
+
+            checkpoints = await compensation.checkpoints(2);
+            expect(checkpoints.toString()).to.be.equal(stableAmount3);
+
             // 4. 3 users call compensation
+
+
             // 5. claim
             // 6. remove unused tokens
         });
