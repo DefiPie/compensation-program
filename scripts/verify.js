d50fdd73a6c5d07653beabcc0b777afe2b506302
maxxttor <maxxttor@gmail.com>
2021-09-27 16:01:53 +0200

    upd verify script

--- scripts/verify.js
+++ scripts/verify.js
@@ -23,45 +23,57 @@ async function main() {
 
     let compensation_startTimestamp = process.env.START_TIMESTAMP_COMPENSATION_RINKEBY;
     let compensation_endTimestamp = process.env.END_TIMESTAMP_COMPENSATION_RINKEBY;
+    let convert_startTimestamp = process.env.START_TIMESTAMP_CONVERT_RINKEBY;
+    let convert_endTimestamp = process.env.END_TIMESTAMP_CONVERT_RINKEBY;
+    let refund_startTimestamp = process.env.START_TIMESTAMP_REFUND_RINKEBY;
+    let refund_endTimestamp = process.env.END_TIMESTAMP_REFUND_RINKEBY;
     let lastApyTimestamp = process.env.RINKEBY_LAST_APY_TIMESTAMP;
+    let calcPoolPrice = process.env.RINKEBY_CALCPOOLPRICE;
+    let controller = process.env.CONTROLLER_RINKEBY;
+    let ETHUSDPriceFeed = process.env.PRICEFEED_RINKEBY;
+    let reservoir = process.env.RINKEBY_RESERVOIR;
+    let compensation_stableCoin = process.env.STABLECOIN_RINKEBY;
+    let rewardApy = process.env.REWARD_APY_RINKEBY;
+    let convert_pTokenFrom = process.env.PTOKENFROM_CONVERT_RINKEBY;
+    let convert_tokenTo = process.env.TOKENTO_CONVERT_RINKEBY;
 
     // Compensation
     await hre.run("verify:verify", {
-        address: '0x14afac35F561121CDCE3719BC6A6F1f5bd7304f3',
+        address: '0x09995C68735034C1AB1a3FD961452a3cEfaC6232',
         constructorArguments: [
-            "0x1834002dba23c27fe9acd947bdb229c699cc012c",
+            compensation_stableCoin,
             compensation_startTimestamp,
             compensation_endTimestamp,
-            "0xd204be259f703503ef2ea03eb401ce6e07254d96",
-            "0x8a753747a1fa494ec906ce90e9f37563a8af630e",
-            "250000000000000000",
+            controller,
+            ETHUSDPriceFeed,
+            rewardApy,
             lastApyTimestamp
         ],
     });
 
     // Convert
     await hre.run("verify:verify", {
-        address: '0xd4897b75aCB0211582E276502fCb7a3cb4099C3B',
+        address: '0xAAeE36e885302784E4BcF268D3Da1bfE4ab7607E',
         constructorArguments: [
-            "0x82aa580e4dDE8e750363962d057feA6FFb138b57",
-            "0xCA03cEd418ECC76191FE2E86f0ffe6dD94d9Fa33",
-            compensation_startTimestamp,
-            compensation_endTimestamp,
-            "0xd204be259f703503ef2ea03eb401ce6e07254d96",
-            "0x8a753747a1fa494ec906ce90e9f37563a8af630e",
-            "0x32b230795AB78AB51EA7b3F15a23a762aaECd3e0"
+            convert_pTokenFrom,
+            convert_tokenTo,
+            convert_startTimestamp,
+            convert_endTimestamp,
+            controller,
+            ETHUSDPriceFeed,
+            reservoir
         ],
     });
 
     // Refund
     await hre.run("verify:verify", {
-        address: '0xd92534f16E02eaCc5E4057AE8496F1EC853A2337',
+        address: '0x59Fce3dA37A888e8500604a1c3021010F9d4496C',
         constructorArguments: [
-            compensation_startTimestamp,
-            compensation_endTimestamp,
-            "0xd204be259f703503ef2ea03eb401ce6e07254d96",
-            "0x8a753747a1fa494ec906ce90e9f37563a8af630e",
-            "0x394b8B1a2C65b08f0b1FbeB2b20fCe4De7213fc9"
+            refund_startTimestamp,
+            refund_endTimestamp,
+            controller,
+            ETHUSDPriceFeed,
+            calcPoolPrice
         ],
     });
 }
