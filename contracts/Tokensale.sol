f91e2ba3d1161e62c1695a6d1a36afaac5b499a3
maxxttor <maxxttor@gmail.com>
2021-10-19 13:12:50 +0200

    claim for getTimeStamp() >= startTimestamp

--- contracts/Tokensale.sol
+++ contracts/Tokensale.sol
@@ -113,7 +113,7 @@ contract Tokensale is Transfers, Ownable {
     }
 
     function claim() public returns (bool) {
-        require(getTimeStamp() > startTimestamp, "Exchange::claim: bad timing for the request");
+        require(getTimeStamp() >= startTimestamp, "Exchange::claim: bad timing for the request");
         uint amount = calcClaimAmount(msg.sender);
 
         if (amount == 0) {
